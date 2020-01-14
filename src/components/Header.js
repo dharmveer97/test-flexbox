9b6ce2f8df61b8e7ea3e8fb222ca4346a3fb74b1
dharmveer97 <dvbangar8@gmail.com>
2020-01-14 13:35:15 +0530

    test tiles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,45 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import SimpleSlider from './SimpleSlider';
-
-const Section = styled.div`
-  background-image: url(/images/background@2x.png);
-`;
+const Section = styled.div``;
 
 const Header = () => (
-  <Section className="section is-large">
+  <Section className="section">
     <div className="container">
-      <div className="columns is-centered">
-        <div className="column is-8">
-          <SimpleSlider />
+      <div className="tile is-ancestor">
+        <div className="tile is-parent">
+          <div className="tile is-child box">
+            <p className="title">Three</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+              semper diam at erat pulvinar, at pulvinar felis blandit.
+              Vestibulum volutpat tellus diam, consequat gravida libero rhoncus
+              ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta
+              orci, quis semper odio felis ut quam.
+            </p>
+            <p>
+              Suspendisse varius ligula in molestie lacinia. Maecenas varius
+              eget ligula a sagittis. Pellentesque interdum, nisl nec interdum
+            </p>
+          </div>
+        </div>
+        <div className="tile is-6 is-vertical is-parent">
+          <div className="tile is-child box">
+            <p className="title">One</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+              non massa sem. Etiam finibus odio quis feugiat facilisis.
+            </p>
+          </div>
+          <div className="tile is-child box">
+            <p className="title">Two</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas
+              non massa sem. Etiam finibus odio quis feugiat facilisis.
+            </p>
+          </div>
         </div>
       </div>
     </div>
