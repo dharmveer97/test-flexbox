14c9e23a7c484405ce123169a1c78ced77498a8d
dharmveer97 <dvbangar8@gmail.com>
2020-01-25 14:57:38 +0530

    minor fixes

--- src/components/CalendarSlider.js
+++ src/components/CalendarSlider.js
@@ -1,47 +1,87 @@
 import React, { Component } from 'react';
 import Slider from 'react-slick';
 
-export default class VariableWidth extends Component {
+import styled from 'styled-components';
+
+const Section = styled.div`
+  background-color: red;
+`;
+
+export default class Responsive extends Component {
   render() {
     const settings = {
-      className: 'slider variable-width',
-      dots: true,
-      infinite: true,
-      centerMode: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      variableWidth: true,
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
     };
     return (
-      <div>
-        <h2>Variable width</h2>
-        <Slider {...settings}>
-          <div style={{ width: 100 }}>
-            <figure className="image is-128x128">
-              <img src="/images/1@2x.png" alt="" />
-            </figure>
+      <Section className="section is-normal">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-10">
+              <div>
+                <h1 className="title is-1 has-text-centered"> ARCHIVE </h1>
+                <Slider {...settings}>
+                  <div>
+                    <figure className="image is-128x128">
+                      <img src="/images/1@2x.png" alt="" />
+                    </figure>
+                  </div>
+                  <div>
+                    <figure className="image is-128x128">
+                      <img src="/images/1@2x.png" alt="" />
+                    </figure>
+                  </div>
+                  <div>
+                    <figure className="image is-128x128">
+                      <img src="/images/1@2x.png" alt="" />
+                    </figure>
+                  </div>
+                  <div>
+                    <figure className="image is-128x128">
+                      <img src="/images/1@2x.png" alt="" />
+                    </figure>
+                  </div>
+                  <div>
+                    <figure className="image is-128x128">
+                      <img src="/images/1@2x.png" alt="" />
+                    </figure>
+                  </div>
+                </Slider>
+              </div>
+            </div>
           </div>
-          <div style={{ width: 100 }}>
-            <figure className="image is-128x128">
-              <img src="/images/1@2x.png" alt="" />
-            </figure>
-          </div>
-          <div style={{ width: 100 }}>
-            <figure className="image is-128x128">
-              <img src="/images/1@2x.png" alt="" />
-            </figure>
-          </div>
-          <div style={{ width: 300 }}>
-            <p>300</p>
-          </div>
-          <div style={{ width: 225 }}>
-            <p>225</p>
-          </div>
-          <div style={{ width: 175 }}>
-            <p>175</p>
-          </div>
-        </Slider>
-      </div>
+        </div>
+      </Section>
     );
   }
 }
