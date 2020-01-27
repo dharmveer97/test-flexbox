dbd8e9391cb960ad3da483c67bcf7f28e9ec9420
dharmveer97 <dvbangar8@gmail.com>
2020-01-27 11:45:51 +0530

    fix slider rows

--- src/components/CalendarSlider.js
+++ src/components/CalendarSlider.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Slider from 'react-slick';
-
 import styled from 'styled-components';
 
 const Section = styled.div`
@@ -10,38 +9,14 @@ const Section = styled.div`
 export default class CalendarSlider extends Component {
   render() {
     const settings = {
-      dots: false,
-      infinite: false,
+      className: 'center',
+      centerMode: true,
+      infinite: true,
+      centerPadding: '30px',
+      slidesToShow: 3,
       speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: false,
-          },
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2,
-          },
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
-        },
-      ],
+      rows: 1,
+      slidesPerRow: 2,
     };
     return (
       <Section className="section is-normal">
@@ -51,6 +26,27 @@ export default class CalendarSlider extends Component {
               <div>
                 <h1 className="title is-1 has-text-centered"> ARCHIVE </h1>
                 <Slider {...settings}>
+                  <div>
+                    <img src="/images/1@2x.png" alt="" />
+                  </div>
+                  <div>
+                    <img src="/images/1@2x.png" alt="" />
+                  </div>
+                  <div>
+                    <img src="/images/1@2x.png" alt="" />
+                  </div>
+                  <div>
+                    <img src="/images/1@2x.png" alt="" />
+                  </div>
+                  <div>
+                    <img src="/images/1@2x.png" alt="" />
+                  </div>
+                  <div>
+                    <img src="/images/1@2x.png" alt="" />
+                  </div>
+                  <div>
+                    <img src="/images/1@2x.png" alt="" />
+                  </div>
                   <div>
                     <img src="/images/1@2x.png" alt="" />
                   </div>
