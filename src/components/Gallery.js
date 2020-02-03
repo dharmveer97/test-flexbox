766687e9101dbae4d0d95554f56d801fc1e13d67
dharmveer97 <dvbangar8@gmail.com>
2020-02-03 20:18:07 +0530

    test map function

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,46 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Section = styled.div``;
+const Section = styled.div`
+  margin-top: 4rem;
+`;
+
+const images = [
+  '/images/photo-6@2x.png',
+  '/images/photo-6@2x.png',
+  '/images/photo-6@2x.png',
+  '/images/photo-6@2x.png',
+  '/images/photo-6@2x.png',
+  '/images/photo-6@2x.png',
+];
 
 class Gallery extends React.Component {
   render() {
-    const {} = this.props;
     return (
       <Section className="">
-        <div className="columns is-variable is-1 is-multiline">
-          <div className="column is-4">
-            <figure className="image image is-1by1">
-              <img
-                src="https://bulma.io/images/placeholders/128x128.png"
-                alt=""
-              />
-            </figure>
-          </div>
-          <div className="column is-4">
-            <figure className="image image is-1by1">
-              <img
-                src="https://bulma.io/images/placeholders/128x128.png"
-                alt=""
-              />
-            </figure>
-          </div>
-          <div className="column is-4">
-            <figure className="image image is-1by1">
-              <img
-                src="https://bulma.io/images/placeholders/128x128.png"
-                alt=""
-              />
-            </figure>
-          </div>
-          <div className="column is-4">
-            <figure className="image image is-1by1">
-              <img
-                src="https://bulma.io/images/placeholders/128x128.png"
-                alt=""
-              />
-            </figure>
-          </div>
+        <div className="columns is-variable is-3 is-multiline">
+          {images.map(items => (
+            <div className="column is-4">
+              <figure className="image is-5by4">
+                <img src={items} alt="gallery" />
+              </figure>
+            </div>
+          ))}
         </div>
       </Section>
     );
