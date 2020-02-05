bedaaee331793e509b673375548dbcec083ebc47
dharmveer97 <dvbangar8@gmail.com>
2020-02-05 15:57:43 +0530

    add frames

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -2,9 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Section = styled.section`
-  background-image: url('/images/backgrounx.png');
-  .columns.is-centered {
-    margin-top: 31rem;
+  background-image: url('/images/two.png');
+  background-size: cover;
+`;
+
+const MainContainer = styled.div`
+  background-image: url(/images/one.png);
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  .section {
+    padding: 7rem 1.5rem !important;
   }
 `;
 
@@ -12,23 +19,30 @@ const HomeHero = () => (
   <Section className="section">
     <div className="container">
       <figure className="image">
-        <img src="/images/title-banner@2x.png" alt="" />
+        <img src="/images/bunt.png" alt="" />
       </figure>
-      <div className="columns  is-centered">
-        <div className="column is-10 has-text-centered">
-          <h1 className="title is-2 has-text-white has-text-weight-semibold	">
-            Get in Touch!
-          </h1>
-          <h1 className="title is-3 has-text-white has-text-weight-semibold	">
-            <span className="has-text-grey-lighter has-text-weight-light ">
-              Email:
-            </span>
-            enquiries@deckerdence.com{' '}
-            <span className="has-text-grey-lighter has-text-weight-light">
-              OR Call:
-            </span>
-            01675 463555
-          </h1>
+      <div className="columns">
+        <MainContainer className="column is-7">
+          <section className="section  is-medium">
+            <div className="container">
+              <h1 className="title is-2 has-text-white has-text-weight-semibold	is-spaced top-text">
+                WEDDINGS
+              </h1>
+              <h1 className="subtitle is-5 has-text-white has-text-weight-normal top-text">
+                Say yes to the perfect wedding venue. Have your special day at
+                our event space in the Warwickshire countryside or take our
+                vintage mobile venue to a location of your choosing. Whether you
+                want to go traditional with silver service or festival-style
+                with the best street food vendors, we can create your dream
+                wedding.
+              </h1>
+            </div>
+          </section>
+        </MainContainer>
+
+        <div className="column is-1" />
+        <div className="column is-4">
+          <p>sds</p>
         </div>
       </div>
     </div>
