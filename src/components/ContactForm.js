0688b4fe303a187d733d178f4eebabb56dc18125
dharmveer97 <dvbangar8@gmail.com>
2020-03-22 19:46:45 +0530

    add contact form on netlify

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -19,10 +19,44 @@ const Section = styled.div`
   }
 `;
 
-const ContactForm = () => (
-  <Section className="section is-block is-relative">
-    <div className="container">
-      <div className="section">
+const encode = data => {
+  return Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+};
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '', phone: '' };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => {
+        this.setState({
+          email: '',
+          phone: '',
+        });
+        alert('Success!');
+      })
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { email, phone } = this.state;
+    return (
+      <Section className="section">
         <div className="container">
           <div className="columns is-centered">
             <div className="column is-7">
@@ -32,41 +66,51 @@ const ContactForm = () => (
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div className="columns is-centered">
-      <div className="column is-6">
-        <div className="field">
-          <div className="control">
-            <input
-              className="input is-medium has-text-whites"
-              type="email"
-              placeholder="Email"
-            />
-          </div>
+        <div className="columns is-centered">
+          <div className="column is-6">
+            <form
+              data-netlify="true"
+              name="contact"
+              onSubmit={this.handleSubmit}
+            >
+              <div className="field">
+                <div className="control">
+                  <input
+                    name="email"
+                    className="input is-medium has-text-white"
+                    value={email}
+                    type="email"
+                    placeholder="Your email"
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <input
+                    name="phone"
+                    className="input is-medium has-text-white"
+                    type="tel"
+                    value={phone}
+                    placeholder="Your phone number"
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
+              <p className="subtitle is-4 has-text-centered">
+                By submitting this form you agree to our contact T & C
+              </p>
+              <div className="has-text-centered">
+                <button type="submit" className="button is-danger">
+                  DOWNLOAD
+                </button>
+              </div>
+            </form>
+          </div>{' '}
         </div>
-        <div className="field">
-          <div className="control">
-            <input
-              name="phone"
-              className="input is-medium has-text-white"
-              type="tel"
-              placeholder="Your phone number"
-            />
-          </div>
-        </div>
-
-        <p className="subtitle is-4 has-text-centered">
-          By submitting this form you agree to our contact T & C
-        </p>
-        <div className="has-text-centered">
-          <button type="button" className="button is-danger">
-            DOWNLOAD
-          </button>
-        </div>
-      </div>
-    </div>
-  </Section>
-);
+      </Section>
+    );
+  }
+}
 
 export default ContactForm;
