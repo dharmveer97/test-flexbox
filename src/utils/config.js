e7b9a62ad69bf815260abee8a27baf56af132393
dharmveer97 <dvbangar8@gmail.com>
2020-04-27 22:51:32 +0530

    minor fixes.

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,14 +6,12 @@ module.exports = {
   description: 'A starting point using ReactJs, bundled with awesome GatsbyJs.',
   siteUrl: 'http://kickoff-gatsbyjs.netlify.com',
   logo: '/images/logo-1024.png',
-
   telephone: '+1-123-123-123',
   email: 'john@doe.com',
   location: 'California, USA',
   twitter: '@NAME-HERE',
   fbUserId: '@NAME-HERE',
   fbAppID: '@NAME-HERE',
-
   type: 'website',
   googleAnalytics: '',
   backgroundColor: '#e0e0e0',
