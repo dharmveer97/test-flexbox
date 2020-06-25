88b0e736b02d1dd345250de50d73cb676ecc75a7
iamkunall <103kunalsharma@gmail.com>
2020-06-25 21:36:26 +0530

    mail chip integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       resolve: 'gatsby-plugin-mailchimp',
       options: {
         endpoint:
-          'https://gmail.us10.list-manage.com/subscribe/post?u=f532190aaeed769106e1f8c4c&amp;id=b574cd5adc"', // string; add your MC list endpoint here; see instructions below
+          'https://gmail.us10.list-manage.com/subscribe/post?u=dd9ecdec0e9fc56c1c26b16c8&amp;id=1ec867ab81', // string; add your MC list endpoint here; see instructions below
         timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import addToMailchimp from 'gatsby-plugin-mailchimp';
 
@@ -20,98 +20,75 @@ const Section = styled.div`
   }
 `;
 
-const encode = data => {
-  return Object.keys(data)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join('&');
-};
-
-class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { email: '', phone: '' };
-  }
-
-  /* Here’s the juicy bit for posting the form submission */
-
-  handleSubmit = e => {
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', ...this.state }),
-    })
-      .then(() => {
-        this.setState({
-          email: '',
-          phone: '',
-        });
-        alert('Success!');
-      })
-      .catch(error => alert(error));
-
+const ContactForm = () => {
+  const [email, setEmail] = useState('');
+  const handleSubmit = e => {
     e.preventDefault();
+
+    addToMailchimp(email)
+      .then(data => {
+        alert(data.result);
+      })
+      .catch(error => {
+        console.log('fail', error);
+      });
   };
 
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  const handleEmailChange = event => {
+    setEmail(event.currentTarget.value);
+  };
 
-  render() {
-    const { email, phone } = this.state;
-    return (
-      <Section className="section">
-        <div className="container">
-          <div className="columns is-centered">
-            <div className="column is-7">
-              <figure className="image">
-                <img src="/images/icon@2x.png" alt="" />
-              </figure>
-            </div>
-          </div>
-        </div>
+  return (
+    <Section className="section">
+      <div className="container">
         <div className="columns is-centered">
-          <div className="column is-6">
-            <form
-              data-netlify="true"
-              name="contact"
-              onSubmit={this.handleSubmit}
-            >
-              <div className="field">
-                <div className="control">
-                  <input
-                    name="email"
-                    className="input is-medium has-text-white"
-                    value={email}
-                    type="email"
-                    placeholder="Your email"
-                    onChange={this.handleChange}
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <div className="control">
-                  <input
-                    name="phone"
-                    className="input is-medium has-text-white"
-                    type="tel"
-                    value={phone}
-                    placeholder="Your phone number"
-                    onChange={this.handleChange}
-                  />
-                </div>
-              </div>
-              <p className="subtitle is-4 has-text-centered">
-                By submitting this form you agree to our contact T & C
-              </p>
-              <div className="has-text-centered">
-                <button type="submit" className="button is-danger">
-                  DOWNLOAD
-                </button>
-              </div>
-            </form>
+          <div className="column is-7">
+            <figure className="image">
+              <img src="/images/icon@2x.png" alt="" />
+            </figure>
           </div>
         </div>
-      </Section>
-    );
-  }
-}
+      </div>
+      <div className="columns is-centered">
+        <div className="column is-6">
+          <form onSubmit={handleSubmit}>
+            <div className="field">
+              <div className="control">
+                <input
+                  name="email"
+                  className="input is-medium has-text-white"
+                  value={email}
+                  type="email"
+                  placeholder="Your email"
+                  onChange={handleEmailChange}
+                />
+              </div>
+            </div>
+            {/* <div className="field">
+              <div className="control">
+                <input
+                  name="phone"
+                  className="input is-medium has-text-white"
+                  type="tel"
+                  value={phone}
+                  placeholder="Your phone number"
+                  onChange={this.handleChange}
+                />
+              </div>
+            </div> */}
+            <p className="subtitle is-4 has-text-centered">
+              By submitting this form you agree to our contact T & C
+            </p>
+            <div className="has-text-centered">
+              <button type="submit" className="button is-danger">
+                DOWNLOAD
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </Section>
+  );
+};
 
 export default ContactForm;
