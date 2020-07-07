8e66d92eeb81ef1d09e8c6c8970c5e92e88e7031
dharmveer97 <dvbangar8@gmail.com>
2020-07-07 13:29:12 +0530

    minor fixes.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,48 +8,6 @@ module.exports = {
     siteUrl: config.siteUrl,
   },
   plugins: [
-<<<<<<< HEAD
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `posts`,
-    //     path: `${__dirname}/src/content/posts/`,
-    //   },
-    // },
-=======
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        defaultLayouts: {
-          default: require.resolve('./src/components/PageLayout.js'),
-        },
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1035,
-              sizeByPixelDensity: true,
-            },
-          },
-        ],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-mailchimp',
-      options: {
-        endpoint:
-          'https://gmail.us10.list-manage.com/subscribe/post?u=dd9ecdec0e9fc56c1c26b16c8&amp;id=1ec867ab81', // string; add your MC list endpoint here; see instructions below
-        timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/content/posts/`,
-      },
-    },
->>>>>>> 88b0e736b02d1dd345250de50d73cb676ecc75a7
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
