b34179839e90f41a0c056874fa26da26d00deeb1
dharmveer97 <dvbangar8@gmail.com>
2021-02-28 20:05:06 +0530

    fix

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const Video = ({ videoSrcURL, videoTitle, ...props }) => (
-  <div className="video">
-    <iframe
-      src={videoSrcURL}
-      title={videoTitle}
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      frameBorder="0"
-      webkitallowfullscreen="true"
-      mozallowfullscreen="true"
-      allowFullScreen
-    />
-  </div>
-);
-export default Video;
+const MyVideo = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="card">
+          <video controls width="250">
+            <source
+              src="https://radio.sikhnet.com/accounts/channel9/live"
+              type="video/webm"
+            />
+          </video>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MyVideo;
